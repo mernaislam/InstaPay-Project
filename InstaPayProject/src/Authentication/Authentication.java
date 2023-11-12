bbdcd29f4e81c60996e8c6d6cafd52239f8c9cd4
Merna Islam <101946404+mernaislam@users.noreply.github.com>
2023-11-12 21:13:04 +0200

    Basic run function

--- InstaPayProject/src/Authentication/Authentication.java
+++ InstaPayProject/src/Authentication/Authentication.java
@@ -1,4 +1,25 @@
 package Authentication;
 
 public class Authentication {
+    // public Account user;
+    // OTPManager otp;
+    // DataManager dm;
+
+    public boolean register(){
+        return true;
+    }
+    public boolean login(String username, String password){
+        return true;
+    }
+
+    public void logout(){}
+
+    public boolean checkAuth(){
+        return true;
+    }
+
+    public boolean usernameExists(String username){
+        return true;
+    }
+
 }
---
+++ InstaPayProject/src/Authentication/OTPManager.java
@@ -0,0 +1,13 @@
+package Authentication;
+
+import java.util.Random;
+
+public class OTPManager {
+    public int getOTP(String mobNum){
+        Random rnd = new Random();
+        return 100000 + rnd.nextInt(900000);
+    }
+    public boolean checkOTP(){
+        return true;
+    }
+}
